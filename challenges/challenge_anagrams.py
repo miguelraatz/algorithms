4996350f34317c4475d5e9e85fd005fbe5122eff
miguelraatz <miguelraatz_@hotmail.com>
2023-07-07 16:04:57 -0300

    feat: implemented function logic for requirement 5

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,31 +1,31 @@
-def merge_sort(string, start=0, end=None):
+def merge_sort(list_string, start=0, end=None):
     if end is None:
-        end = len(string)
+        end = len(list_string)
     if (end - start) > 1:
         mid = (start + end) // 2
-        merge_sort(string, start, mid)
-        merge_sort(string, mid, end)
-        merge(string, start, mid, end)
+        merge_sort(list_string, start, mid)
+        merge_sort(list_string, mid, end)
+        merge(list_string, start, mid, end)
 
 
-def merge(string, start, mid, end):
-    left = string[start:mid]
-    right = string[mid:end]
+def merge(list_string, start, mid, end):
+    left = list_string[start:mid]
+    right = list_string[mid:end]
 
     left_index, right_index = 0, 0
 
     for general_index in range(start, end):
         if left_index >= len(left):
-            string[general_index] = right[right_index]
+            list_string[general_index] = right[right_index]
             right_index = right_index + 1
         elif right_index >= len(right):
-            string[general_index] = left[left_index]
+            list_string[general_index] = left[left_index]
             left_index = left_index + 1
         elif left[left_index] < right[right_index]:
-            string[general_index] = left[left_index]
+            list_string[general_index] = left[left_index]
             left_index = left_index + 1
         else:
-            string[general_index] = right[right_index]
+            list_string[general_index] = right[right_index]
             right_index = right_index + 1
 
 
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or any(
+        not isinstance(num, int) or num < 0 for num in nums
+    ):
+        return False
+
+    nums_sorted = sorted(nums)
+
+    for num in range(1, len(nums_sorted)):
+        if nums_sorted[num] == nums_sorted[num - 1]:
+            return nums_sorted[num]
+
+    return False
