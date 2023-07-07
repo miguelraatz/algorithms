3c7d36ad874936869ad853696754898ddf0e3768
miguelraatz <miguelraatz_@hotmail.com>
2023-07-07 13:36:59 -0300

    test: implemented tests requirement 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "testing"
+    assert encrypt_message(message, 4) == "gni_tset"
+    assert encrypt_message(message, 2) == "gnits_et"
+    assert encrypt_message(message, -2) == "gnitset"
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 2)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("testing", 5.523)
