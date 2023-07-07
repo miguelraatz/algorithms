d409bb62d17427cbc0102767ad7cac0b59386475
miguelraatz <miguelraatz_@hotmail.com>
2023-07-07 16:28:24 -0300

    feat: implemented function logic for requirement 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word=""):
+    if not word or word == "":
+        return False
+    for index in range(len(word)):
+        if word[index] != word[len(word) - index - 1]:
+            return False
+
+    return True
